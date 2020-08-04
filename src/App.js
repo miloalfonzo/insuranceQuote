e862b5d5db1917e64b1f074db18f2d0b79a04657
Milagros Alfonzo <milagrosalfonzo@outlook.com.ar>
2020-08-04 19:21:25 -0300

    added form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import Header from "./components/Header";
+import Form from "./components/Form";
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: auto;
+`;
+
+const ContForm = styled.div`
+  background-color: #ffff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Header title="Cotizador de seguros" />
-    </div>
+      <ContForm>
+        <Form />
+      </ContForm>
+    </Container>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ContCamp = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  --webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+
+const Button = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #ffff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 0.5s ease;
+  margin-top: 2rem;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #26c6da;
+  }
+`;
+
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
+function Form() {
+  const [data, setData] = useState({
+    brand: "",
+    year: "",
+    plan: ""
+  });
+  const [error, setError] = useState(false);
+
+  const { brand, year, plan } = data;
+
+  const getInfo = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (brand.trim() === "" || year.trim() === "" || plan.trim === "") {
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    //get difference betweeen years
+
+    //each year is 3% cheaper
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {error ? <Error>Todos los campos son obligatorios</Error> : null}
+        <ContCamp>
+          <Label>Marca</Label>
+          <Select name="brand" value={brand} onChange={getInfo}>
+            <option value="">--Selecciones --</option>
+            <option value="americano">Americano</option>
+            <option value="europeo">Europeo</option>
+            <option value="asiatico">Asiatico</option>
+          </Select>
+        </ContCamp>
+        <ContCamp>
+          <Label>Año</Label>
+          <Select name="year" value={year} onChange={getInfo}>
+            <option value="">-- Seleccione --</option>
+            <option value="2021">2021</option>
+            <option value="2020">2020</option>
+            <option value="2019">2019</option>
+            <option value="2018">2018</option>
+            <option value="2017">2017</option>
+            <option value="2016">2016</option>
+            <option value="2015">2015</option>
+            <option value="2014">2014</option>
+            <option value="2013">2013</option>
+            <option value="2012">2012</option>
+          </Select>
+        </ContCamp>
+        <ContCamp>
+          <Label>Plan</Label>
+          <InputRadio
+            type="radio"
+            name="plan"
+            value="basic"
+            checked={plan === "basic"}
+            onChange={getInfo}
+          />
+          Básico
+          <InputRadio
+            type="radio"
+            name="plan"
+            value="full"
+            checked={plan === "full"}
+            onChange={getInfo}
+          />
+          Completo
+        </ContCamp>
+        <Button type="submit">Cotizar</Button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
